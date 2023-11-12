139fdaa6ad72f5070a0aee1a5075ca3189f83014
Tamirucode <tam.egu76@gmail.com>
2023-11-12 23:15:27 +0000

    add home.html to booking app

--- booking/urls.py
+++ booking/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from booking import views
 urlpatterns = [
     
-    path("", views.get_booking_list, name='home'),
+    path('', views.home, name='home'),
     path("mybooking", views.get_booking_list, name='mybooking'),
     path('add', views.add_booking, name='add'),
     
