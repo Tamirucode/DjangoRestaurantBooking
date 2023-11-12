2ea9e1f6ff59a4d156c229f55497cc422e1e2d3b
Tamirucode <tam.egu76@gmail.com>
2023-11-12 22:57:42 +0000

    add url to booking_confirmation.html

--- booking/urls.py
+++ booking/urls.py
@@ -4,6 +4,7 @@ from booking import views
 urlpatterns = [
     
     path("", views.get_booking_list, name='home'),
+    path("mybooking", views.get_booking_list, name='mybooking'),
     path('add', views.add_booking, name='add'),
     
 ]
