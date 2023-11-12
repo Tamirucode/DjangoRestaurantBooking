63c311defa59217dd6ef232a44ca6620525ddcb3
Tamirucode <tam.egu76@gmail.com>
2023-11-12 19:39:59 +0000

    created add_booking.html to booking app

---
+++ booking/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Booking
+from django.forms import  widgets
+
+
+class BookingForm(forms.ModelForm):
+    """
+    Class to construct booking form from the model.
+    """
+    class Meta:
+        """
+        Add form inputs from the model items as form fields.
+        """
+        model = Booking
+        fields = ('name', 'phone_number', 'Number_of_persons', 'booking_date_and_time')
+        widgets = {
+            'booking_date_and_time': widgets.DateTimeInput(attrs={'type': 'datetime-local'})
+        }
+
+   
