e5ad6be5cfb89d760274114645b689c564f80f28
Tamirucode <tam.egu76@gmail.com>
2023-11-12 16:46:55 +0000

    added database model and validation to the project

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils import timezone
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+
+
+class Booking(models.Model):
+    """
+    Class to represent booking model
+    in database and for booking form.
+    """
+    
+    name = models.CharField(max_length=20, null=True)
+    
+    Number_of_persons = models.PositiveIntegerField(
+                            null=True,
+                            validators=[MinValueValidator(1)])
+    phone_number = PhoneNumberField(null=False, default='+44 7814985924')
+    
+    def validate_date(booking_date_and_time):
+        """
+        Function to validate date so that
+        booking date is in the future.
+        """
+        if booking_date_and_time < timezone.now():
+            raise ValidationError("Date and Time cannot be in the past")
+        
+    booking_date_and_time = models.DateTimeField(
+                                null=True,
+                                blank=True,
+                                validators=[validate_date] 
+                               )
+        
+    
+    
+    class Meta:
+        """
+        Class container with metadata
+        attached to the model.
+        """
+        
+        unique_together = [ 'name', 'phone_number']
+        
+    def __str__(self):
+        """
+        Function to return object model
+        boookings as string.
+        """
+        return f' User {self.name} has made a booking \
+                for {self.Number_of_persons} persons\
+                   for {self.booking_date_and_time }'
