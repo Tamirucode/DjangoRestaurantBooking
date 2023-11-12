7bfd1909a00ed9419eca3895d1dc0ed1dbfe61ed
Tamirucode <tam.egu76@gmail.com>
2023-11-12 15:20:08 +0000

    preparing for deployment

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-tamirucode-djangorestau-yqovldqsz0w.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = ['8000-tamirucode-djangorestau-yqovldqsz0w.ws-eu106.gitpod.io', 'restaurantbooking2023-4f0b057bd1a0.herokuapp.com']
 
 
 # Application definition
