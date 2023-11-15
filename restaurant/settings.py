ec94fce9e41695627f9ec195499fb86b74430560
Tamirucode <tam.egu76@gmail.com>
2023-11-15 14:40:08 +0000

    add style to book_list.html

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-tamirucode-djangorestau-yqovldqsz0w.ws-eu106.gitpod.io', 'restaurantbooking2023-4f0b057bd1a0.herokuapp.com']
+ALLOWED_HOSTS = ['8000-tamirucode-djangorestau-yg4d07fwwah.ws-eu106.gitpod.io', 'restaurantbooking2023-4f0b057bd1a0.herokuapp.com']
 
 
 # Application definition
